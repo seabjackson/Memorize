d65c2e1e092764b4ebdadecb0cb8101d7dc10be4
Seab <honeyboyseab@gmail.com>
2020-07-08 01:15:24 -0400

    added Initial project Set up

---
+++ Memorize/Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Seab Jackson on 7/8/20.
+//  Copyright © 2020 Seab Jackson. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
