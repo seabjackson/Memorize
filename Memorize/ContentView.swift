9464a034b3231643458b4cc1667ae010d9b6855e
Seab Jackson <honeyboyseab@gmail.com>
2023-05-03 10:58:06 -0400

    added the UI pieces to show a grid of emojis

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,19 +8,125 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var emojis = ["🚗","🚌","🚁","🚀", "🚘", "🚙", "🛫", "🛵", "🚐",
+                  "🚛", "🚑", "🛴", "🚲", "🚕", "🚚", "🚞", "🚢", "🚒", "🛻", "🚝",
+                  "🚟", "🛺", "🚂", "🚓"]
+    @State var emojiCount = 24
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            ScrollView {
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 64))]) {
+                    ForEach(emojis[0..<emojiCount], id: \.self) { emoji in
+                        CardView(content: emoji)
+                            .aspectRatio(2/3, contentMode: .fit)
+                    }
+                }
+            }
+            .foregroundColor(.red)
+
+            Spacer()
+            HStack {
+                remove
+                Spacer()
+                add
+            }
+            .font(.largeTitle)
+            .padding(.horizontal)
         }
-        .padding()
+        .padding(.horizontal)
+        
+        var remove: some View {
+            Button {
+                if emojiCount > 1 {
+                    emojiCount -= 1
+                }
+            } label: {
+                Image(systemName: "minus.circle")
+            }
+        }
+        
+        var add: Button<Image> {
+            Button {
+                if emojiCount < emojis.count {
+                    emojiCount += 1
+                }
+            } label: {
+                Image(systemName: "plus.circle")
+            }
+        }
+        
     }
 }
+
+struct CardView: View {
+    var content: String
+    @State var isFaceUp: Bool = true
+    
+    var body: some View {
+        ZStack {
+            let shape =  RoundedRectangle(cornerRadius: 20.0)
+            if isFaceUp {
+                shape
+                    .fill()
+                    .foregroundColor(.white)
+                shape                    .strokeBorder(lineWidth: 3)
+                    .foregroundColor(/*@START_MENU_TOKEN@*/.red/*@END_MENU_TOKEN@*/)
+                Text(content)
+                    .font(.largeTitle)
+            } else {
+                shape
+                    .fill()
+            }
+        }
+        .onTapGesture {
+            isFaceUp = !isFaceUp
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
+        ContentView()
+            .preferredColorScheme(.light)
     }
 }
